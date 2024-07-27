37264ff6cfd8df3c8cfcf01a9b7b1c81aa7bee12
Anshumanv36 <anshumanmishra.v36@gmail.com>
2024-07-27 16:35:09 +0530

    initial commit

---
+++ App/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import Options from "./screens/options";
+
+export default () => <Options />; //exporting the optios page //self closing view;
---
+++ App/screens/Home.js
@@ -0,0 +1,12 @@
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, StyleSheet, StatusBar } from "react-native";
+
+import colors from "../constants/colors";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.nplBlue,
+  },
+});
