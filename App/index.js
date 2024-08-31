cecff6c0f31a30576979e72c68b3611c2a23fef5
Anshumanv36 <anshumanmishra.v36@gmail.com>
2024-08-31 22:04:25 +0530

    created a stub API

--- App/index.js
+++ App/index.js
@@ -2,6 +2,15 @@ import React from "react";
 // import Options from "./screens/Options";
 import Home from "./screens/Home";
 import Navigation from "./config/Navigation";
+import { api } from "./util/api";
+
+api("/latest?base=USD")
+  .then((response) => {
+    console.log("response", response);
+  })
+  .catch((error) => {
+    console.log("error", error);
+  });
 
 // export default () => <Home />; //exporting the optios page //self closing view;
 export default () => <Navigation />;
